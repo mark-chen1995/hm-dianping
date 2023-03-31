3ba3c929e190a0304e08fee42d2a4e55a25234f2
markchen <1975016204@qq.com>
2023-03-31 20:30:55 +0800

    add one another line

--- src/main/java/com/hmdp/utils/RedisConstants.java
+++ src/main/java/com/hmdp/utils/RedisConstants.java
@@ -22,6 +22,7 @@ public class RedisConstants {
     public static final String USER_SIGN_KEY = "sign:";
     // public static final String CODE_PREFIX = "code:";
     // add new line
+    // add other line
     /**
      * 无用的方法，用来测试git
      */
