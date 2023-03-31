c5489bfd926a5a96d55c79a2b632abb171361b1a
markchen <1975016204@qq.com>
2023-03-31 20:36:45 +0800

    not comment one line

--- src/main/java/com/hmdp/utils/RedisConstants.java
+++ src/main/java/com/hmdp/utils/RedisConstants.java
@@ -20,7 +20,7 @@ public class RedisConstants {
     public static final String FEED_KEY = "feed:";
     public static final String SHOP_GEO_KEY = "shop:geo:";
     public static final String USER_SIGN_KEY = "sign:";
-    // public static final String CODE_PREFIX = "code:";
+    public static final String CODE_PREFIX = "code:";
     // add new line
     // add other line
     /**
